21d11adde89b4e54ba5e72ef4fd18ea07278a9ec
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-26 19:23:17 +0300

    Task2

---
+++ 7 kyu/Task2/Task2/Task2/TwoToOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class TwoToOne
+    {
+        public string Longest(string s1, string s2)
+        {
+            String s3 = s1 + s2;
+
+            var c = s3.Distinct().OrderBy(x => x).ToArray();
+            string s4 = string.Concat(c);
+
+            return s4;
+        }
+    }
+}
