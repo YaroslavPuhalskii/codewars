9680a5a453917e58575acaec2705c319aa17adb1
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-26 14:07:44 +0300

    Task1

---
+++ 7 kyu/Task1/Task1/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Kata
+    {
+        public int Disemvowel(int n)
+        {
+            List<int> m = new List<int>();
+            while (n != 0)
+            {
+                int i = 10;
+                int d = n % i;
+                int q = d * d;
+
+                m.Add(q);
+                n = (int)n / i;
+
+            }
+            m.Reverse();
+            var g = string.Join("", m);
+            int result = Int32.Parse(g);
+            Console.WriteLine(result);
+
+            return result;
+        }
+    }
+}
---
+++ 7 kyu/Task1/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*Welcome.In this kata, you are asked to square every digit of a number and concatenate them.
+  For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+ Note: The function accepts an integer and returns an integer*/
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+            int n = 128;
+            kata.Disemvowel(n);
+
+            Console.ReadLine();
+        }
+    }
+}
