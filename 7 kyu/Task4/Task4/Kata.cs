380dcf1ba56ce750d0e2182fc6c4d253719e06c7
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-27 11:43:33 +0300

    Task

---
+++ 7 kyu/Task4/Task4/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public  class Kata
+    {
+        public  bool XO(string input)
+        {
+            var c1 = new List<char>();
+            var c2 = new List<char>();
+            foreach (char c in input)
+            {
+                if (c == 'x' || c == 'X') { c1.Add(c); }
+                else if (c == 'o' || c == 'O') { c2.Add(c); }
+            }
+            if (c1.Count == c2.Count || (c1.Count == 0 && c2.Count == 0)) { return true; }
+            else { return false; }
+        }
+    }
+}
---
+++ 7 kyu/Task4/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+
+            kata.XO("ooxx");// true
+            kata.XO("xooxx");// false
+            kata.XO("ooxXm");// true
+            kata.XO("zpzpzpp");// true
+            kata.XO("zzoo");// false
+
+            Console.ReadLine();
+        }
+    }
+}
