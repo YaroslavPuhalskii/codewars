79e76d27b87e9e77d0da94d552285277835a136b
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-27 14:50:02 +0300

    Task

---
+++ 7 kyu/Task5/Task5/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class Kata
+    {
+        public string GetMiddle(string s)
+        {
+            int c = s.Count();
+            int b =  c / 2;
+            if (c % 2 == 1)
+            { 
+                return s.Substring(b,1);
+            }
+            else 
+            { 
+                return s.Substring(b-1,2);
+            } 
+        }
+    }
+}
---
+++ 7 kyu/Task5/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  You are going to be given a word. Your job is to return the middle character of the word.
+    If the word's length is odd, return the middle character. If the word's length is even,
+    return the middle 2 characters.
+
+#Examples:
+Kata.getMiddle("test") should return "es"
+Kata.getMiddle("testing") should return "t"
+Kata.getMiddle("middle") should return "dd"
+Kata.getMiddle("A") should return "A"
+*/
+
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+
+            kata.GetMiddle("test");
+            kata.GetMiddle("testing");
+            kata.GetMiddle("middle");
+            kata.GetMiddle("A");
+
+            Console.ReadLine();
+        }
+    }
+}
