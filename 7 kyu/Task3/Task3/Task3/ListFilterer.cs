0095160c628b0afc7ab7e891b300f9dc94baacf9
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-27 10:36:38 +0300

    Task

---
+++ 7 kyu/Task3/Task3/Task3/ListFilterer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            var c = new List<int>();
+
+            foreach(object i in listOfItems)
+            {
+                if(i is Int32)
+                {
+                    c.Add((int)i);
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ 7 kyu/Task3/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ In this kata you will create a function that takes a list of non-negative integers and strings
+     and returns a new list with the strings filtered out.
+   
+Example
+
+ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"}) => {1, 2}
+ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b", 0, 15}) => {1, 2, 0, 15}
+ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b", "aasf", "1", "123", 231}) => {1, 2, 231}
+*/
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFilterer.GetIntegersFromList(new List<object>() { 1, 2, "a", "b" });
+            ListFilterer.GetIntegersFromList(new List<object>() { 1, 2, "a", "b", 0, 15 });
+            ListFilterer.GetIntegersFromList(new List<object>() { 1, 2, "a", "b", "aasf", "1", "123", 231 });
+            Console.ReadLine();
+        }
+    }
+}
