4c3a5739134abc74a8d6f166793dcddd8d0a374a
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-28 11:44:00 +0300

    Task

---
+++ 7 kyu/Task6/Task6/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class Kata
+    {
+        public int FindShort(string s)
+        {
+            string[] mass = s.Split(',', ' ');
+            string slovo = mass[0];
+            for (int i=0;  i< mass.Length-1;i++)
+            {
+                if(mass[i].Length > mass[i+1].Length)
+                {
+                    if(slovo.Length> mass[i+1].Length)
+                    {
+                        slovo = mass[i + 1];
+                    }
+                }
+            }
+            int b = slovo.Length;
+            return b;
+        }
+    }
+}
---
+++ 7 kyu/Task6/Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+   Simple, given a string of words, return the length of the shortest word(s).
+   String will never be empty and you do not need to account for different data types.
+*/
+
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+            kata.FindShort("hgjhg j ghjvh jj");
+            Console.ReadLine();
+        }
+    }
+}
