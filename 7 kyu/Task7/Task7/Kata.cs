ff62b6ab5cc4ca05c7a4296e2f1944f7b9b8399a
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-28 14:12:55 +0300

    Task

---
+++ 7 kyu/Task7/Task7/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Kata
+    {
+        public static bool IsSquare(int n)
+        {
+            return Math.Sqrt(n) % 1 == 0;
+        }
+    }
+}
---
+++ 7 kyu/Task7/Task7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+   Task
+Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer;
+in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false 
+ */
+
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.IsSquare(3);
+            Kata.IsSquare(25);
+        }
+    }
+}
