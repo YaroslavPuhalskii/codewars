4ad652cb3ce13ff2277d9d89ba4dd5ddadebf63c
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-28 16:22:49 +0300

    Task

---
+++ 7 kyu/Task8/Task8/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+	public static class Kata
+	{
+		public static int sumTwoSmallestNumbers(int[] numbers)
+		{
+			var c = numbers.OrderBy(x =>x).Take(2);
+			int sum = 0;
+			foreach (int p in c)
+			{
+				sum += p;
+			}
+			return sum;
+		}
+	}
+}
---
+++ 7 kyu/Task8/Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive
+integers. No floats or non-positive integers will be passed.
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 1, 2, 3, 4, 1, 5 };
+            Kata.sumTwoSmallestNumbers(n);
+
+            Console.ReadLine();
+        }
+    }
+}
