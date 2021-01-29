c34703d4de159c93aaad7f9b0ada0102385ef5d8
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-29 21:48:09 +0300

    Task

--- 6 kyu/Task3/Task3/Program.cs
+++ 6 kyu/Task3/Task3/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*говно-код - хорошо, что сработал
+Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace the missing
+second character of the final pair with an underscore ('_').
 
+Examples:
+SplitString.Solution("abc"); // should return ["ab", "c_"]
+SplitString.Solution("abcdef"); // should return ["ab", "cd", "ef"]
+*/
 namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SplitString.Solution("abljjkvbklkjhjlv");
+            Console.ReadLine();
         }
     }
 }
--- 6 kyu/Task3/Task3/SplitString.cs
+++ 6 kyu/Task3/Task3/SplitString.cs
@@ -10,8 +10,39 @@ namespace Task3
     {
         public static string[] Solution(string str)
         {
+            List<String> s = new List<String>();
+            string s1= null, s2, sum;
+            var summ = str.Length;
+            string[] str2;
+            for (int i =0; i<str.Length; i++)
+            {
+                if(i%2 ==0)
+                {
+                    s1 = str[i].ToString();
+                }
+                if((i == (str.Length -1)) && summ%2!=0)
+                {
+                    s2 = "_";
+                    sum = s1 + s2;
+                    s.Add(sum);
+                }
+                if (i % 2 == 1) 
+                {
+                    s2 = str[i].ToString();
+                    sum = s1 + s2;
+                    s.Add(sum);
+                }                
+            }
+            int l = 0;
+            str2 = new string[s.Count];
+            foreach (string p in s)
+            {
+                Console.WriteLine(p);
+                str2[l] = p;
+                l++;
+            }
 
-            return null;
+            return str2 ;
         }
     }
 }
