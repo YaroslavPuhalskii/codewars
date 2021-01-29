52901bf3ff066b9524d3cfd92d003bee1e4b1f06
YaroslavPuhalskii <netpywka14@gmail.com>
2021-01-30 00:08:01 +0300

    Task

---
+++ 6 kyu/Task4/Task4/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Kata
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            int i=0;
+            var a = numbers.OrderBy(x => x).Take(3).ToArray();
+            var b = numbers.OrderByDescending(x => x).Take(1).ToArray();
+            if (a[0] == a[1])
+            {
+                i = b[0];
+            }
+            else if (a[0] != a[1]) { i = a[0]; }
+            return i;
+        }
+    }
+}
---
+++ 6 kyu/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+*/
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 1, 1, 1, 2, 1, 1 };
+            List<int> b = new List<int>() { 2, 2, 2, 1, 2, 2 };
+            Kata.GetUnique(a);
+            Kata.GetUnique(b);
+            Console.ReadLine();
+        }
+    }
+}
