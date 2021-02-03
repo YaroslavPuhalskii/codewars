499da25d92a660933f2a6b6e5b8f1f3753e8dea6
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-03 09:54:34 +0300

    Task

---
+++ 5 kyu/Task1/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[,] { { 1, 1, 1 },
+                                        { 0, 2, 2 },
+                                        { 0, 0, 0 } };
+            TicTacToe.IsSolved(board);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5 kyu/Task1/Task1/TicTacToe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class TicTacToe
+    {
+        public static int IsSolved(int[,] board)
+        {
+            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
+            {
+                
+            }
+
+            if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+            {
+                
+            }
+
+            if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+            {
+               
+            }
+
+            if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+            {
+                
+            }
+
+            if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
+            {
+                
+            }
+
+            if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+            {
+                
+            }
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                
+            }
+
+            if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
+            {
+                
+            }
+            return 0;
+        }
+    }
+}
