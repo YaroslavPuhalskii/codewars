018154c0db5ea931c39c76c4e7b163b8d0bd3b88
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-03 11:20:49 +0300

    Task

--- 5 kyu/Task1/Task1/Program.cs
+++ 5 kyu/Task1/Task1/Program.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current
+state is solved, wouldn't we? Our goal is to create a function that will check that for us!
 
+Assume that the board comes in the form of a 3x3 array, where the value is 0 if 
+a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+We want our function to return:
+
+-1 if the board is not yet finished (there are empty spots),
+1 if "X" won,
+2 if "O" won,
+0 if it's a cat's game (i.e. a draw).
+You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+*/
 namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[,] { { 1, 1, 1 },
-                                        { 0, 2, 2 },
-                                        { 0, 0, 0 } };
+            int[,] board = new int[,] { { 2, 0, 2 },
+                                        { 2, 1, 1 },
+                                        { 1, 2, 1 } };
             TicTacToe.IsSolved(board);
             Console.ReadLine();
         }
--- 5 kyu/Task1/Task1/TicTacToe.cs
+++ 5 kyu/Task1/Task1/TicTacToe.cs
@@ -10,46 +10,111 @@ namespace Task1
     {
         public static int IsSolved(int[,] board)
         {
-            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
-            {
-                
-            }
+            int a = -1;int b = 1;int c = 2; int d = 0; int sum =0;
 
-            if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
-            {
-                
-            }
+                if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
+                {
+                    if (board[0, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
-            {
-               
-            }
+                if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+                {
+                    if (board[1, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
-            {
-                
-            }
+                if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+                {
+                    if (board[2, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
-            {
-                
-            }
+                if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+                {
+                    if (board[0, 1] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
-            {
-                
-            }
+                if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
+                {
+                    if (board[0, 0] != 0)
+                    {
+                        if (board[0, 2] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
-            {
-                
-            }
+                if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+                {
+                    if (board[0, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
 
-            if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                {
+                    if (board[0, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
+
+                if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
+                {
+                    if (board[2, 0] != 0)
+                    {
+                        if (board[0, 0] == 1)
+                        { sum = b; }
+                        if (board[0, 0] == 2)
+                        { sum = c; }
+                    }
+                }
+            int count = 0;
+            if(sum == 0)
             {
-                
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == 0)
+                        {
+                            count++;  
+                        }
+                    }
+                }
+                sum = count > 0 ? a : d;
             }
-            return 0;
+            return sum;
         }
     }
 }
