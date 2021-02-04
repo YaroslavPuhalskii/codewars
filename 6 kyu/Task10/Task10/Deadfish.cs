c26be3266844b692c5ad1b3add0946a5566b8a72
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-04 14:39:55 +0300

    Task

---
+++ 6 kyu/Task10/Task10/Deadfish.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    public class Deadfish
+    {
+        public static int[] Parse(string data)
+        {
+            List<char> list = new List<char>();
+            List<int> array = new List<int>();
+            list.AddRange(data);
+            int x = 0;           
+            
+            foreach(var p in list)
+            {
+                switch(p)
+                {
+                    case 'i':
+                        {
+                            x++;  
+                            break;
+                        }
+                    case 'd':
+                        {
+                            x--;
+                            break;
+                        }
+                    case 's':
+                        {
+                            x = x * x;
+                            break;
+                        }
+                    case 'o':
+                        {
+                            array.Add(x);
+                            break;
+                        }
+                }
+            }
+
+
+            return array.ToArray();
+        }
+    }
+}
---
+++ 6 kyu/Task10/Task10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+Deadfish.Parse("iiisdoso") => new int[] {8, 64};
+*/
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "iiisdoso";
+            Deadfish.Parse(str);
+            Console.ReadLine();
+        }
+    }
+}
