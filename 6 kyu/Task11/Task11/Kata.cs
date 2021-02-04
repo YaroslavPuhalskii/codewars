b7494aae8f02e0bc7d8731c7ad6679c1f444e87f
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-04 16:23:33 +0300

    Task

---
+++ 6 kyu/Task11/Task11/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    public class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            var arr = seq.ToList().OrderBy(xo => xo).ToArray();
+            int count = 1; int x = 0;
+            for(int i = 0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                if(i <arr.Count()-1)
+                {
+                    if (arr[i] == arr[i + 1])
+                    { count++; }
+                    if (arr[i] != arr[i + 1] && (count % 2 == 1 || count == 1))
+                    {
+                        x = arr[i];
+                        count = 1;
+                    }
+                    else if (arr[i] != arr[i+1] && count%2==0){ count = 1; }
+                }                
+            }
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+}
---
+++ 6 kyu/Task11/Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, 5, -2, -1 };
+            Kata.find_it(a);
+            Console.ReadLine();
+        }
+    }
+}
