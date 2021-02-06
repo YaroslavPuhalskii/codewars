e610b2df9c67b1aa5de922897337573deb8cb89b
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-06 18:59:55 +0300

    Task

---
+++ 5 kyu/Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 2113469;
+            TimeFormat.GetReadableTime(sec);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5 kyu/Task3/Task3/TimeFormat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Task3
+{
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            String str = null;
+            int a = seconds / 3600; String a1 = a < 10 ? "0"+a.ToString() : a.ToString();
+            int b = (seconds - (a * 3600))/60; String b1 = b < 10 ? "0" + b.ToString() : b.ToString();
+            int c = seconds - (a * 3600) - (b * 60); String c1 = c < 10 ? "0" + c.ToString() : c.ToString();
+            if (a > 99) { str = "99:59:59"; }
+            else { str = a1+":" + b1+":" + c1; }
+            
+            return str;
+        }
+    }
+}
