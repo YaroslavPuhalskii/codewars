1b840e92ab826ade1607eac79d999478aaa686f6
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-06 23:20:29 +0300

    Task IP-address

---
+++ 5 kyu/ListTask/ListTask/CountIPAddresses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Implement a function that receives two IPv4 addresses, and returns the number of addresses between
+them (including the first one, excluding the last one).
+
+All inputs will be valid IPv4 addresses in the form of strings.
+The last address will always be greater than the first one.
+*/
+namespace ListTask
+{
+    public class CountIPAddresses
+    {
+        public static long IpsBetween(string start, string end)
+        {
+            
+            var a = start.Split('.').ToList().Select(x => int.Parse(x));
+            var b = end.Split('.').ToList().Select(x => int.Parse(x));
+            var sum1 = a.ElementAt(3) 
+                + a.ElementAt(2) * 256
+                + a.ElementAt(1) * 256 * 256
+                + a.ElementAt(0) * 256 * 256 * 256;
+            var sum2 = b.ElementAt(3) 
+                + b.ElementAt(2) * 256
+                + b.ElementAt(1) * 256 * 256
+                + b.ElementAt(0) * 256 * 256 * 256;
+            long result = sum2 - sum1;
+            return result;
+        }
+    }
+}
---
+++ 5 kyu/ListTask/ListTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String start = "10.0.0.0"; String end = "10.0.1.0";
+            CountIPAddresses.IpsBetween(start, end);
+            Console.ReadLine();
+        }
+    }
+}
