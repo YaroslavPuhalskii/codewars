558a41a756463238c64c4b31c5595ce44cd919a2
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-08 12:25:58 +0300

    Task BigInteger

--- 4 kyu/Task2/Task2/Kata.cs
+++ 4 kyu/Task2/Task2/Kata.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Numerics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,44 +12,19 @@ namespace Task2
     {
         public static string sumStrings(string a, string b)
         {
-            CultureInfo culture;
-            String qqq = null;
-            double a1 = 0; double b1 = 0; double sum = 0;
-            if (a != null && a != "")
-            {
-                a1 = Convert.ToDouble(a);
-            }
-            else { a = "0"; a1 = Convert.ToDouble(a); }
+            BigInteger BigIntA = new BigInteger();
+            BigInteger BigIntB = new BigInteger();
+            BigInteger result = new BigInteger();
 
-            if (b != null && b != "")
-            {
-                b1 = Convert.ToDouble(b);
-            }
-            else { b = "0"; b1 = Convert.ToDouble(b); }
+            if ( a == "") { a = "0"; }
+            if ( b == "") { b = "0"; }
 
-            sum = a1 + b1;
-            Console.WriteLine(sum);
-            culture = CultureInfo.CreateSpecificCulture("en-CA");
-            Console.WriteLine(sum.ToString(qqq, culture));
-            Console.WriteLine("{0:F3}", sum);
+            BigIntA = BigInteger.Parse(a, NumberStyles.Integer);
+            BigIntB = BigInteger.Parse(b, NumberStyles.Integer);
 
-            //var m = (sum == 0) ? 1 : (int)Math.Ceiling(Math.Log10(Math.Abs(sum) + 0.5));
-            //List<int> list = new List<int>();
-            //for (int i = 0; i < m; i++)
-            //{
-            //    int q = (int)sum % 10;
-            //    list.Add(q);
-            //    sum = sum / 10;
+            result = BigIntA + BigIntB;
 
-            //}
-            //list.Reverse();
-            //String ex = String.Join("", list);
-            //foreach(var p in list)
-            //{
-            //    Console.WriteLine(p);
-            //}
-            Console.WriteLine(qqq);
-            return qqq;
+            return result.ToString();            
         }
     }
 }
