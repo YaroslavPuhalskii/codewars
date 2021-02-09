b425d7fa2b59bc6ca0db56e7fce1233d7765207b
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-09 18:52:08 +0300

    Task

---
+++ 5 kyu/ListTask/ListTask/Kata2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    public static class Kata2
+    {
+        public static int TrailingZeros(int n)
+        {
+            
+        }
+    }
+}
