a76d3e5899ecb5692f9c67d8db34fcd8cba41090
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-13 15:36:26 +0300

    Line

---
+++ 5 kyu/ListTask/ListTask/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    public class Line
+    {
+        public static string WhoIsNext(string[] names, long n)
+        {
+            long multiplicator = 1;
+            var l = names.Length;
+            while(n>multiplicator* l)
+            {
+                n -= l * multiplicator;
+                multiplicator *= 2;
+            }
+            var m = (n - 1) / multiplicator;
+            return names[m];
+        }
+    }
+}
--- 5 kyu/ListTask/ListTask/Program.cs
+++ 5 kyu/ListTask/ListTask/Program.cs
@@ -10,36 +10,40 @@ namespace ListTask
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-            Kata5.MaxSequence(arr);
+            string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+            int n = 40;
+            Line.WhoIsNext(names, n);
 
-                //var a = new List<object>() { 1, 2, 3, 4, 5, 6 };
-                //var k = 20;
-                //Josephus.JosephusPermutation(a,k);
+            //int[] arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            //Kata5.MaxSequence(arr);
 
-                //string word = "abba";
-                //List<string> str = new List<string>() { "aabb", "abcd", "bbaa", "dada" };
-                //Kata4.Anagrams(word, str);
+            //var a = new List<object>() { 1, 2, 3, 4, 5, 6 };
+            //var k = 20;
+            //Josephus.JosephusPermutation(a,k);
 
-                //int[] arr = new int[] { 2, 4, 4, 5, 4 };
-                //Kata3.Score(arr);
+            //string word = "abba";
+            //List<string> str = new List<string>() { "aabb", "abcd", "bbaa", "dada" };
+            //Kata4.Anagrams(word, str);
 
-                //int n = 1000;
-                //Kata2.TrailingZeros(n);
+            //int[] arr = new int[] { 2, 4, 4, 5, 4 };
+            //Kata3.Score(arr);
 
-                //int n = 25;
-                //FactDecomp.Decomp(n);
+            //int n = 1000;
+            //Kata2.TrailingZeros(n);
 
-                //int[] arr = new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
-                //Kata.MoveZeroes(arr);
+            //int n = 25;
+            //FactDecomp.Decomp(n);
 
-                //int lst = 7775460;
-                //PrimeDecomp.factors(lst);
+            //int[] arr = new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+            //Kata.MoveZeroes(arr);
 
-                //String start = "10.0.0.0"; String end = "10.0.1.0";
-                //CountIPAddresses.IpsBetween(start, end);
+            //int lst = 7775460;
+            //PrimeDecomp.factors(lst);
 
-                Console.ReadLine();
+            //String start = "10.0.0.0"; String end = "10.0.1.0";
+            //CountIPAddresses.IpsBetween(start, end);
+
+            Console.ReadLine();
         }        
     }
 }
