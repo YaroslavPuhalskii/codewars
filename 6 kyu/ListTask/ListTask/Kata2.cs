29954adcb45267ed61fbd2679d8f13b0e2625501
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-21 15:36:56 +0300

    ArrayDiff

---
+++ 6 kyu/ListTask/ListTask/Kata2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Your goal in this kata is to implement a difference function, which subtracts one
+list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+*/
+namespace ListTask
+{
+    public class Kata2
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+
+            var hash = new HashSet<int>(b);
+            return a.Where(x => !hash.Contains(x)).ToArray();
+        }
+    }
+}
--- 6 kyu/ListTask/ListTask/Program.cs
+++ 6 kyu/ListTask/ListTask/Program.cs
@@ -10,10 +10,13 @@ namespace ListTask
     {
         static void Main(string[] args)
         {
-            int[] exampleTest1 = { 2, 6, 8, -10, 3 };
-            Kata1.Find(exampleTest1);
+            Kata2.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 });
+
+            //int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+            //Kata1.Find(exampleTest1);
 
             //Persist.Persistence(987);
+            Console.ReadLine();
         }
     }
 }
