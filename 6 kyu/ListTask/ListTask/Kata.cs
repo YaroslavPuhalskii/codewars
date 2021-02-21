26d35084da6f1f0f4ea82ab996276201ef8e01e4
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-21 13:35:11 +0300

    Task

---
+++ 6 kyu/ListTask/ListTask/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative,
+return 0(for languages that do have them)
+*/
+namespace ListTask
+{
+    public static class Kata
+    {
+        public static int Solution(int n)
+        {
+            return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+    }
+}
