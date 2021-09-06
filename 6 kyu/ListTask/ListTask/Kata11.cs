ac4ecb87dc77dfe0343ef1653ad4c264e6798753
yaroslav <netpywka14@gmail.com>
2021-09-06 10:16:54 +0300

    .

---
+++ 6 kyu/ListTask/ListTask/Kata11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    public class Kata11 
+    {
+        public static string BreakCamelCase(string str)
+        {
+            var result = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((int)str[i] >= 60 && (int)str[i] <= 90)
+                {
+                    result += " ";
+                }
+
+                result += str[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ 6 kyu/ListTask/ListTask/Kata12.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    public class Kata12
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            var tower = new List<string>(nFloors);
+
+            for (var currFloor = 1; currFloor <= nFloors; currFloor++)
+            {
+                var padding = new string(' ', nFloors - currFloor);
+                var blocks = new string('*', ((currFloor - 1) * 2) + 1);
+                tower.Add($"{padding}{blocks}{padding}");
+            }
+            return tower.ToArray();
+        }
+    }
+}
